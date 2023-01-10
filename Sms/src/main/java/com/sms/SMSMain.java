1d01831bca7611690f3bb3cac837930ef70114e1
BeatriceTrivellone <beatrice.trivellone@student.univaq.it>
2023-01-10 18:04:46 +0100

    fix sms

--- Sms/src/main/java/com/sms/SMSMain.java
+++ Sms/src/main/java/com/sms/SMSMain.java
@@ -8,8 +8,8 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SMSMain {
-    public static final String ACCOUNT_SID = "AC5d8ef93d3df7d4966ba3d11926eaed67";
-    public static final String AUTH_TOKEN = "669780a13dd40079461616622f11fd93";
+    public static final String ACCOUNT_SID = "";
+    public static final String AUTH_TOKEN = "";
     public static void main(String[] args) {
         Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
         SpringApplication.run(SMSMain.class, args);
