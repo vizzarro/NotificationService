f8d99040fe7b1631c5c30bae4cad254562b50a05
FrancescaDiCesare <francesca.dicesare@aesys.tech>
2023-06-01 10:42:23 +0200

    added EmailSenderRequestDTOS, Message and Receiver class

---
+++ NotificationRequest/src/main/java/com/notificationrequest/model/Message.java
@@ -0,0 +1,60 @@
+package com.notificationrequest.model;
+
+import jakarta.persistence.Embedded;
+
+public class Message {
+
+    String sender;
+    @Embedded
+    Receiver receiver;
+
+    String subject;
+
+    String filePath;
+
+    String text;
+
+    public Message() {
+
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public Receiver getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(Receiver receiver) {
+        this.receiver = receiver;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+}
---
+++ NotificationRequest/src/main/java/com/notificationrequest/model/Receiver.java
@@ -0,0 +1,46 @@
+package com.notificationrequest.model;
+
+import jakarta.persistence.Embeddable;
+
+import java.util.List;
+
+@Embeddable
+public class Receiver {
+    private List<String> addressBCC;
+    private List<String> addressCC;
+    private List<String> address;
+
+    public Receiver(List<String> addressBCC, List<String> addressCC, List<String> address) {
+        this.addressBCC = addressBCC;
+        this.addressCC = addressCC;
+        this.address = address;
+    }
+
+    public Receiver() {
+
+    }
+
+    public List<String> getAddressBCC() {
+        return addressBCC;
+    }
+
+    public void setAddressBCC(List<String> addressBCC) {
+        this.addressBCC = addressBCC;
+    }
+
+    public List<String> getAddressCC() {
+        return addressCC;
+    }
+
+    public void setAddressCC(List<String> addressCC) {
+        this.addressCC = addressCC;
+    }
+
+    public List<String> getAddress() {
+        return address;
+    }
+
+    public void setAddress(List<String> address) {
+        this.address = address;
+    }
+}
